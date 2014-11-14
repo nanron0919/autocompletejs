4d21e8ff7ade10e056e332abcb8e5ee70f7784a0
Norman Chen <nanron0919@gmail.com>
2014-11-14 18:11:06 +0800

    Update autocomplete.js

--- assets/js/autocomplete.js
+++ assets/js/autocomplete.js
@@ -1,144 +1,171 @@
 'use strict';
 
-var selector = function(syntax, base_el) {
-        var elem = (base_el || document),
-            elems = elem.querySelectorAll(syntax),
-            result = [];
-
-        for (var i = 0; i < elems.length; i++) {
-            result.push(elems.item(i));
-        }
-
-        return 1 === result.length ? result[0] : result;
-    },
-    attachEvent = function(type, elem, func) {
-        var eventListener = document.addEventListener || document.attachEvent;
-
-        if (false === elem instanceof Array) {
-            elem = [elem];
-        }
-
-        elem.forEach(function(el, k) {
-            eventListener.apply(el, [type, func]);
-        });
-    },
-    fetchRemoteData = function(url, keyword, func) {
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', url + '?q=' + keyword, true);
-        xhr.responseType = 'json';
-        xhr.onreadystatechange = function(res) {
-            // done
-            if (4 === this.readyState) {
-                func(this.response);
+var Autocomplete = (function(document) {
+    if ('undefined' === typeof [].forEach) {
+        Array.prototype.forEach = function(func) {
+            for (var i = 0; i < this.length; i++) {
+                func(this[i], i);
             }
         };
-        xhr.send();
-    },
-    removeResultList = function(basenode) {
-        var existing_ul = selector('.autocomplete-list', basenode);
-        existing_ul = existing_ul instanceof Array ? existing_ul : [existing_ul];
-
-        if (0 < existing_ul.length) {
-            existing_ul.forEach(function(el, k) {
-                el.remove();
-            });
-        }
-    },
-    attachResultList = function(basenode, data) {
-        var frag = document.createDocumentFragment(),
-            ul = document.createElement('ul'),
-            parent = basenode.parentNode,
-            li;
-
-        removeResultList(parent);
-
-        if (0 < data.length) {
-            setClassName(ul, 'autocomplete-list');
-            ul.style.width = basenode.offsetWidth + 'px';
-
-            data.forEach(function(el, k) {
-                li = document.createElement('li');
-                li.textContent = el;
-
-                frag.appendChild(li);
-            });
-
-            ul.appendChild(frag);
-
-            parent.appendChild(ul);
-        }
-    },
-    moveHighlight = function(base, dir) {
-        var list = selector('.autocomplete-list', base.parentNode),
-            list_item, active_index, target_item;
-
-        if ('undefined' === typeof list.length) {
-            list_item = selector('li', list);
-            list_item = list_item instanceof Array ? list_item : [list_item];
-            active_index = list.dataset.active_index || -1;
-
-            setClassName(list_item, '');
-
-            switch (dir) {
-            case 'up':
-                active_index--;
-                break;
-            case 'down':
-                active_index++;
-                break;
-            }
-
-            active_index = (0 > active_index ? list_item.length - 1 : active_index);
-            active_index = (list_item.length <= active_index ? 0 : active_index);
-
-            list.dataset.active_index = active_index;
-            target_item = list_item[active_index];
-
-            setClassName(target_item, 'active');
-
-            base.value = target_item.textContent;
-        }
-    },
-    setClassName = function(elems, class_name) {
-        elems = (elems instanceof Array ? elems : [elems]);
-        elems.forEach(function(el, k) {
-            el.className = class_name;
-        });
-    },
-    form = selector('.autocomplete-form'),
-    keyword = selector('.keyword', form);
-
-attachEvent('submit', form, function(e) {
-    e.preventDefault();
-});
-
-attachEvent('keyup', keyword, function(e) {
-    var self = e.currentTarget,
-        key_code = e.keyCode;
-
-    // up and down
-    if (38 === key_code || 40 === key_code) {
-        moveHighlight(self, 38 === key_code ? 'up' : 'down');
     }
-    // return
-    else if (13 === key_code) {
-        removeResultList(self.parentNode);
-    }
-    else {
-        if ('' !== self.value) {
-            fetchRemoteData(
-                form.action,
-                self.value,
-                function(data) {
-                    data = data.filter(function(el) {
-                        return self.value.toLowerCase() === el.substr(0, self.value.length).toLowerCase();
-                    });
-                    attachResultList(self, data);
+
+    return function(form, callback) {
+        callback = callback || function() {};
+
+        var selector = function(syntax, base_el) {
+                var elem = (base_el || document),
+                    elems = elem.querySelectorAll(syntax),
+                    result = [];
+
+                for (var i = 0; i < elems.length; i++) {
+                    result.push(elems.item(i));
                 }
-            );
-        }
-        else {
-            removeResultList(self.parentNode);
-        }
-    }
-});
+
+                return 1 === result.length ? result[0] : result;
+            },
+            attachEvent = function(type, elem, func) {
+                var eventListener = document.addEventListener || document.attachEvent;
+
+                if (false === elem instanceof Array) {
+                    elem = [elem];
+                }
+
+                elem.forEach(function(el, k) {
+                    eventListener.apply(el, [type, func]);
+                });
+            },
+            autocomplete = {
+                fetchRemoteData: function(url, keyword, func) {
+                    var xhr = new XMLHttpRequest();
+                    xhr.open('GET', url + '?q=' + keyword, true);
+                    xhr.responseType = 'json';
+                    xhr.onreadystatechange = function(res) {
+                        // done
+                        if (4 === this.readyState) {
+                            func(this.response || this.responseText);
+                        }
+                    };
+                    xhr.send();
+                },
+                getListElement: function(basenode) {
+                    return selector('.autocomplete-list', basenode);
+                },
+                removeResultList: function(basenode) {
+                    var existing_ul = autocomplete.getListElement(basenode);
+                    existing_ul = existing_ul instanceof Array ? existing_ul : [existing_ul];
+
+                    if (0 < existing_ul.length) {
+                        existing_ul.forEach(function(el, k) {
+                            if ('undefined' !== typeof el.remove) {
+                                el.remove();
+                            }
+                            else {
+                                el.removeNode();
+                            }
+                        });
+                    }
+                },
+                attachResultList: function(basenode, data) {
+                    var frag = document.createDocumentFragment(),
+                        ul = document.createElement('ul'),
+                        parent = basenode.parentNode,
+                        li;
+
+                    if ('string' === typeof data) {
+                        data = JSON.parse(data);
+                    }
+
+                    if (0 < data.length) {
+                        autocomplete.setClassName(ul, 'autocomplete-list');
+                        ul.style.width = basenode.offsetWidth + 'px';
+
+                        data.forEach(function(el, k) {
+                            li = document.createElement('li');
+                            li.textContent = el;
+
+                            frag.appendChild(li);
+                        });
+
+                        ul.appendChild(frag);
+
+                        parent.appendChild(ul);
+                    }
+                },
+                moveHighlight: function(base, dir) {
+                    var list = autocomplete.getListElement(base.parentNode),
+                        list_item, active_index, target_item;
+
+                    if ('undefined' === typeof list.length) {
+                        list_item = selector('li', list);
+                        list_item = list_item instanceof Array ? list_item : [list_item];
+                        active_index = list.dataset.active_index || -1;
+
+                        autocomplete.setClassName(list_item, '');
+
+                        switch (dir) {
+                        case 'up':
+                            active_index--;
+                            break;
+                        case 'down':
+                            active_index++;
+                            break;
+                        }
+
+                        active_index = (0 > active_index ? list_item.length - 1 : active_index);
+                        active_index = (list_item.length <= active_index ? 0 : active_index);
+
+                        list.dataset.active_index = active_index;
+                        target_item = list_item[active_index];
+
+                        autocomplete.setClassName(target_item, 'active');
+
+                        base.value = target_item.textContent;
+                    }
+                },
+                setClassName: function(elems, class_name) {
+                    elems = (elems instanceof Array ? elems : [elems]);
+                    elems.forEach(function(el, k) {
+                        el.className = class_name;
+                    });
+                },
+                keyword: selector('.keyword', form)
+            };
+
+        attachEvent('submit', form, function(e) {
+            e.preventDefault();
+        });
+
+        attachEvent('keyup', autocomplete.keyword, function(e) {
+            var self = e.currentTarget,
+                key_code = e.keyCode;
+
+            // up and down
+            if (38 === key_code || 40 === key_code) {
+                autocomplete.moveHighlight(self, 38 === key_code ? 'up' : 'down');
+            }
+            // return
+            else if (13 === key_code) {
+                autocomplete.removeResultList(self.parentNode);
+            }
+            else {
+                if ('' !== self.value) {
+
+                    autocomplete.fetchRemoteData(
+                        form.action,
+                        self.value,
+                        function(data) {
+                            autocomplete.removeResultList(self.parentNode);
+                            autocomplete.attachResultList(self, data);
+                            callback(data, self, autocomplete.getListElement(self.parentNode));
+                        }
+                    );
+                }
+                else {
+                    autocomplete.removeResultList(self.parentNode);
+                }
+            }
+        });
+
+        return;
+    };
+})(document);
